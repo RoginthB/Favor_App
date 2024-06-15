e7c0d770adbf0d366523e7d409819f03253d84d2
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-15 17:36:47 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,146 +1,161 @@
-import { Avatar, Box, Container, IconButton, Typography, Stack, ImageList, ImageListItem, Button } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import ProfileIcon from '@mui/icons-material/Person'
-import LoginRounded from '@mui/icons-material/LoginRounded'
-import Add from '@mui/icons-material/Add'
-import { Link, useNavigate } from 'react-router-dom';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
-import axios from 'axios';
+import {
+  Avatar,
+  Box,
+  Container,
+  IconButton,
+  Typography,
+  Stack,
+  ImageList,
+  ImageListItem,
+  
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+//import axios from "axios";
+import store from "../store";
+//import { useDispatch } from "react-redux";
+import { deleteData } from "../reducers/appReducers";
+import LoginPage from "./LoginPage";
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
 function Profile() {
-  const [userLogedIn, setUserLogedIn] = useState(JSON.parse(localStorage.getItem('userLogdIn')) || false);
+  const [userLogedIn, setUserLogedIn] = useState(false);
   const [userDetails, setUserDetails] = useState({});
   const navigate = useNavigate();
-  const handleGetUserData = (email) => {
-    //const userdata = JSON.parse(localStorage.getItem('userDetails'))
-    //console.log(userdata)
+  //const dispatch = useDispatch();
+  useEffect(() => {
+    setUserDetails(store.getState().loginUserDetails.user);
+    if (Object.keys(userDetails).length !== 0) {
+      setUserLogedIn(true);
+    }
+  }, [userDetails]);
 
-    axios.get(process.env.REACT_APP_API_URL+"/user/login/" + email).then((response) => {
-      setUserDetails(response.data);
-    })
-
-
-  }
+  // const handleGetUserData = (email) => {
+  //   axios
+  //     .get(process.env.REACT_APP_API_URL + "/user/login/" + email)
+  //     .then((response) => {
+  //       setUserDetails(response.data);
+  //       store.dispatch(getNewData(response.data));
+  //     });
+  //   console.log(userDetails);
+  // };
   const handelLogOut = () => {
     setUserLogedIn(false);
-    localStorage.clear();
-  }
-
-  useEffect((userLogedIn) => {
-    if (userLogedIn) {
-      handleGetUserData(JSON.parse(localStorage.getItem("userDetails")).email);
-    }
-
-  }, [])
-
-  const profileData = [{ no: userDetails.post, name: 'Post' }, { no: userDetails.followers, name: 'Followers' }, { no: userDetails.following, name: 'Following' }]
-
+    store.dispatch(deleteData());
+  };
   return (
     <>
-      <Container maxWidth='sm' sx={{ marginTop: '80px', marginBottom: '80px' }}>
-        <Typography component='p' variant='h4' textAlign='center' color='primary'>
-          Profile
-        </Typography>
-        {!userLogedIn &&
-          <Box maxWidth='sm' textAlign='center' >
-            <Button variant="contained" component={Link} to="/CreateUser" color='primary' sx={{ margin: '24px' }} endIcon={<Add />}>
-              New User</Button>
-            <Button variant="contained" color='secondary' component={Link} to="/Login" sx={{ margin: '24px' }} endIcon={<LoginRounded />}>
-              Login</Button>
-          </Box>
-        }
-        {userLogedIn &&
+      {
+        !userLogedIn && <LoginPage />
+      }
+      {userLogedIn && (
+        <Container
+          maxWidth="sm"
+          sx={{ marginTop: "80px", marginBottom: "80px" }}
+        >
+          <Typography
+            component="p"
+            variant="h4"
+            textAlign="center"
+            color="primary"
+          >
+            Profile
+          </Typography>
           <Box>
-            <Stack direction='row' alignItems='center' justifyContent='space-between' sx={{ width: '100%', margin: 'auto' }}>
-              <Stack direction='row' alignItems='center' justifyContent='start' sx={{width:'80%'}}>
-              <Avatar sx={{ backgroundColor: '#497174', width: '80px', height: '80px', margin: '10px' }} >
-                <ProfileIcon fontSize='large' />
-              </Avatar>
-              <Box sx={{ padding: '20px' }}>
-                <Typography component='p' variant='h5' noWrap={true}>
-                  {userDetails.name}
-                </Typography>
-                <Typography component='p' variant='p'>
-                  {userDetails.userId}
-                </Typography>
-              </Box>
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between"
+              sx={{ width: "100%", margin: "auto" }}
+            >
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="start"
+                sx={{ width: "80%" }}
+              >
+                <Avatar
+                  sx={{
+                    backgroundColor: "#497174",
+                    width: "80px",
+                    height: "80px",
+                    margin: "10px",
+                    fontSize:'40px'
+                  }}
+                >
+                  {userDetails.name[0]}
+                </Avatar>
+                <Box sx={{ padding: "20px" }}>
+                  <Typography component="p" variant="h5" noWrap={true}>
+                    {userDetails.name}
+                  </Typography>
+                  <Typography component="p" variant="p">
+                    {userDetails.userId}
+                  </Typography>
+                </Box>
               </Stack>
-              <IconButton edge='center' aria-label='LogoutRoundedIcon' onClick={() => { navigate("/login"); handelLogOut(); }}>
-                <LogoutRoundedIcon fontSize='large' sx={{ width:'24px', color: 'secondary', "& :hover": { color: "primary" } }} />
+              <IconButton
+                edge="center"
+                aria-label="LogoutRoundedIcon"
+                onClick={() => {
+                  navigate("/login");
+                  handelLogOut();
+                }}
+              >
+                <LogoutRoundedIcon
+                  fontSize="large"
+                  sx={{
+                    width: "24px",
+                    color: "secondary",
+                    "& :hover": { color: "primary" },
+                  }}
+                />
               </IconButton>
             </Stack>
-            <Stack direction='row' alignItems='center' justifyContent='space-around' spacing={2} sx={{ backgroundColor: '#eff5f5', paddingBottom: '20px', borderRadius: '20px', margin: 'auto' }}>
-              {profileData.map((data) => {
-                return (<Stack direction='column' alignItems='center'>
-                  <IconButton color='primary'>
-                    <Typography sx={{ padding: '20px', fontWeight: 600 }} color='primary'>
-                      {data.no}
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-around"
+              spacing={2}
+              sx={{
+                backgroundColor: "#eff5f5",
+                paddingBottom: "20px",
+                borderRadius: "20px",
+                margin: "auto",
+              }}
+            >
+              {[
+                { no: userDetails.post.length, name: "Post" },
+                { no: userDetails.followers.length, name: "Followers" },
+                { no: userDetails.following.length, name: "Following" },
+              ].map((data,index) => {
+                return (
+                  <Stack direction="column" alignItems="center" key={index}>
+                    <IconButton color="primary">
+                      <Typography
+                        sx={{ padding: "20px", fontWeight: 600 }}
+                        color="primary"
+                      >
+                        {data.no}
+                      </Typography>
+                    </IconButton>
+                    <Typography color="primary" sx={{ fontWeight: 300 }}>
+                      {data.name}
                     </Typography>
-                  </IconButton>
-                  <Typography color='primary' sx={{ fontWeight: 300 }}>
-                    {data.name}
-                  </Typography>
-                </Stack>)
+                  </Stack>
+                );
               })}
             </Stack>
-            <Box sx={{ marginTop: '24px', marginBottom: '84px' }}>
-              <ImageList sx={{ width: '100%', height: 'auto' }} cols={3} >
-                {itemData.map((item) => (
-                  <ImageListItem key={item.img}>
+            <Box sx={{ marginTop: "24px", marginBottom: "84px" }}>
+              <ImageList sx={{ width: "100%", height: "auto" }} cols={3}>
+                {userDetails.post.map((item) => (
+                  <ImageListItem key={item._id}>
                     <img
-                      src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                      srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                    className={item.userUniqId}
+                      id={item._id}
+                      src={`${item.imagePath}?w=164&h=164&fit=crop&auto=format`}
+                      srcSet={`${item.imagePath}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                       alt={item.title}
                       loading="lazy"
                     />
@@ -148,10 +163,11 @@ function Profile() {
                 ))}
               </ImageList>
             </Box>
-          </Box>}
-      </Container>
+          </Box>
+        </Container>
+      )}
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
