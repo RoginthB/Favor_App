4048f0f096cefe7331dcc368cf5c926b07e85c5b
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-15 17:35:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-      <App />
+        <App />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
