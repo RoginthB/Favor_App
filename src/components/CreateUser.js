ca806fbad6702b483cda024e21f300de22d369aa
Roginth B <55707004+RoginthB@users.noreply.github.com>
2024-06-15 17:43:34 +0530

    Update CreateUser.js

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,8 +1,7 @@
-
 // src/components/UserRegistration.js
-import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import {
   TextField,
   Button,
@@ -16,20 +15,21 @@ import {
   Grid,
   InputAdornment,
   IconButton,
-} from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-
+  Backdrop,
+  CircularProgress,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const CreateUser = () => {
   const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    dateOfBirth: '',
-    gender: '',
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    dateOfBirth: "",
+    gender: "",
   });
-
+  const [open, setOpen] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -52,144 +52,203 @@ const CreateUser = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add logic to handle user registration (e.g., send data to server or perform authentication)
-    try{
-      
-    const formData = {
-    name:userData.name,
-    email:userData.email,
-    password:userData.password,
-    dataOfBirth:userData.dateOfBirth,
-    gender:userData.gender,
-    userId:userData.name+userData.dateOfBirth[8]+userData.dateOfBirth[9]
-  };
-    
-     axios.post(process.env.REACT_APP_API_URL+'/user/newUser',formData).then((res)=>{
-      console.log(res.status);
-    })
-    //console.log('Image uploaded:', response.data);
-    alert(`${formData.name} your account is created sccussefuly !!`);
-    navigate('/Login');
-    console.log('User data submitted:', userData);
+    setOpen(true);
+    try {
+      const formData = {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        dataOfBirth: userData.dateOfBirth,
+        gender: userData.gender,
+        userId:
+          userData.name + userData.dateOfBirth[8] + userData.dateOfBirth[9],
+      };
+
+      axios
+        //.post("http://localhost:5000/user/newUser", formData)
+        .post(process.env.REACT_APP_API_URL + "/user/newUser", formData)
+        .then((res) => {
+          if(res.status ===200){
+            setOpen(false);
+            alert(`${formData.name} your account is created sccussefuly !!`);
+            navigate("/Login");
+          }else if( res.status ===400){
+            setOpen(false);
+            alert(`${formData.name } this email Id we have already. !!`);
+            navigate("/Login");
+          }else{
+            setOpen(false);
+            alert(`something went roung ):`);
+          }
+        });
     } catch (error) {
-        console.error('Error:', error);
-      }
+      setOpen(false);
+      console.error("Error:", error);
+    }
   };
 
   return (
-    <Grid
-    container
-    alignItems='center'
-    justifyContent='center'
-    style={{minHeight:'100vh'}}
-    >
-    <Container component="main" maxWidth="md">
-      <Paper elevation={3} style={{ padding: 20, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h5">User Registration</Typography>
-        <form onSubmit={handleSubmit} style={{ width: '100%', marginTop: 20 }}>
-          <Grid container spacing={2} columns={16}>
-            <Grid item xs={8}>
-              <TextField
+    <>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Container component="main" maxWidth="md">
+          <Paper
+            elevation={3}
+            style={{
+              padding: 20,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5">User Registration</Typography>
+            <form
+              onSubmit={handleSubmit}
+              style={{ width: "100%", marginTop: 20 }}
+            >
+              <Grid container spacing={2} columns={16}>
+                <Grid item xs={8}>
+                  <TextField
+                    fullWidth
+                    label="Name"
+                    name="name"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    value={userData.name}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <TextField
+                    fullWidth
+                    label="Email"
+                    name="email"
+                    type="email"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    value={userData.email}
+                    onChange={handleChange}
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <TextField
+                    fullWidth
+                    label="Password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    value={userData.password}
+                    onChange={handleChange}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handlePasswordVisibility}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <TextField
+                    fullWidth
+                    label="Confirm Password"
+                    name="confirmPassword"
+                    type={showConfirmPassword ? "text" : "password"}
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    value={userData.confirmPassword}
+                    onChange={handleChange}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleConfirmPasswordVisibility}
+                            edge="end"
+                          >
+                            {showConfirmPassword ? (
+                              <VisibilityOff />
+                            ) : (
+                              <Visibility />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <TextField
+                    fullWidth
+                    label="Date of Birth"
+                    name="dateOfBirth"
+                    type="date"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    value={userData.dateOfBirth}
+                    onChange={handleChange}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={8}>
+                  <FormControl
+                    fullWidth
+                    variant="outlined"
+                    margin="normal"
+                    required
+                  >
+                    <InputLabel id="gender-label">Gender</InputLabel>
+                    <Select
+                      label="Gender"
+                      name="gender"
+                      value={userData.gender}
+                      onChange={handleChange}
+                    >
+                      <MenuItem value="male">Male</MenuItem>
+                      <MenuItem value="female">Female</MenuItem>
+                      <MenuItem value="other">Other</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
                 fullWidth
-                label="Name"
-                name="name"
-                variant="outlined"
-                margin="normal"
-                required
-                value={userData.name}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                fullWidth
-                label="Email"
-                name="email"
-                type="email"
-                variant="outlined"
-                margin="normal"
-                required
-                value={userData.email}
-                onChange={handleChange}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                fullWidth
-                label="Password"
-                name="password"
-                type={showPassword ? 'text' : 'password'}
-                variant="outlined"
-                margin="normal"
-                required
-                value={userData.password}
-                onChange={handleChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handlePasswordVisibility} edge="end">
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                fullWidth
-                label="Confirm Password"
-                name="confirmPassword"
-                type={showConfirmPassword ? 'text' : 'password'}
-                variant="outlined"
-                margin="normal"
-                required
-                value={userData.confirmPassword}
-                onChange={handleChange}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handleConfirmPasswordVisibility} edge="end">
-                        {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                fullWidth
-                label="Date of Birth"
-                name="dateOfBirth"
-                type="date"
-                variant="outlined"
-                margin="normal"
-                required
-                value={userData.dateOfBirth}
-                onChange={handleChange}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <FormControl fullWidth variant="outlined" margin="normal" required>
-                <InputLabel id="gender-label">Gender</InputLabel>
-                <Select label="Gender" name="gender" value={userData.gender} onChange={handleChange}>
-                  <MenuItem value="male">Male</MenuItem>
-                  <MenuItem value="female">Female</MenuItem>
-                  <MenuItem value="other">Other</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
-          <Button type="submit" variant="contained" color="primary" fullWidth style={{ marginTop: 20 }}>
-            Register
-          </Button>
-        </form>
-      </Paper>
-    </Container>
-    </Grid>
+                style={{ marginTop: 20 }}
+              >
+                Register
+              </Button>
+            </form>
+          </Paper>
+        </Container>
+      </Grid>
+
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        //onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
   );
 };
 
